2dd8754b32e3c1ccc5ed0daef59d74f1fa328c98
Ryan <euphrates1orange@gmail.com>
2011-06-16 09:47:41 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newquill::Application.config.secret_token = '9fb41efbbb004f317e8794a8202f0657297ca8fe236a00f153e81c8151df15e953777d9e0b3c46dc05d1031ab3cb1be862b8e12ed97f303c3eb009271ce8eda1'
