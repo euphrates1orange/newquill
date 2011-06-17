337548ebaf3c1fe0b915c9a248026b5c1ceef083
Ryan <euphrates1orange@gmail.com>
2011-06-17 17:57:27 -0400

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Newquill::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
-  get "pages/help"
+  match '/about', :to => 'pages#about'
+  match '/contact', :to => 'pages#contact'
+  match '/help', :to => 'pages#help'
+end
+  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -60,4 +61,3 @@ Newquill::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
-end
